15412081ed1486f01a057074b92c1a66429c115e
gyanpatra <gyan101@gmail.com>
2014-07-14 21:00:45 -0700

    fixed the documet ready issue

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
-function letterChanges() { 
-   var msg = $("#updateable").val();
-    
-   console.log( msg );
-  
-  // declate the result; 
-   var result = "";
-    
-  // for (initial condition, true condition, operation)
-  for (var i=0; i<msg.length; i++) {
-    var letter = msg[i];
-    var cc = msg.charCodeAt(i);
-    var nextcc = cc+1;                 
-    var nextletter = String.fromCharCode(nextcc);
-
-    console.log(nextletter);
+$(document).ready(function() {
+  function letterChanges() { 
+     var msg = $("#updateable").val();
       
-    if("aeiou".indexOf(nextletter.toLowerCase())!=-1){
-        nextletter = nextletter.toUpperCase();
+     console.log( msg );
+    
+    // declate the result; 
+     var result = "";
+      
+    // for (initial condition, true condition, operation)
+    for (var i=0; i<msg.length; i++) {
+      var letter = msg[i];
+      var cc = msg.charCodeAt(i);
+      var nextcc = cc+1;                 
+      var nextletter = String.fromCharCode(nextcc);
+
+      console.log(nextletter);
+        
+      if("aeiou".indexOf(nextletter.toLowerCase())!=-1){
+          nextletter = nextletter.toUpperCase();
+      }
+        
+      result += nextletter;
     }
-      
-    result += nextletter;
+    
+    $("#result").html(result);
+    return result;          
   }
-  
-  $("#result").html(result);
-  return result;          
-}
 
-$("#LetterChanges").click(letterChanges);
+  $("#LetterChanges").click(letterChanges);
+});
